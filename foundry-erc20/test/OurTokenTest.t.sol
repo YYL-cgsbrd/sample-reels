34cfe049d7654fe7a687d6409e8fd9e8017f1e89
YYL-cgsbrd <yyl11223300@qq.com>
2026-03-26 20:07:57 +0800

    feat: add foundry-erc20 project to repository

---
+++ foundry-erc20/test/OurTokenTest.t.sol
@@ -0,0 +1,50 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployOurToken} from "../script/DeployOurToken.s.sol";
+import {OurToken} from "../src/OurToken.sol";
+
+contract OurTokenTest is Test {
+    OurToken public ourToken;
+    DeployOurToken public deployer;
+
+    address bob = makeAddr("bob");
+    address alice = makeAddr("alice");
+
+    uint256 public constant STARTING_BALANCE = 1000 ether;
+
+    function setUp() public {
+        deployer = new DeployOurToken();
+        ourToken = deployer.run();
+        console.log(
+            "Test contract balance:",
+            ourToken.balanceOf(address(this))
+        );
+        console.log("msg.sender balance:", ourToken.balanceOf(msg.sender));
+
+        vm.prank(msg.sender);
+        ourToken.transfer(bob, STARTING_BALANCE);
+    }
+
+    function testBobBalance() public {
+        assertEq(STARTING_BALANCE, ourToken.balanceOf(bob));
+    }
+
+    function testAllowancesWorks() public {
+        uint256 initinalAllowance = 1000;
+
+        //Bob approves Alice to spend tokens on her behalf
+        vm.prank(bob);
+        ourToken.approve(alice, initinalAllowance);
+
+        uint256 transferAmount = 500;
+
+        vm.prank(alice);
+        ourToken.transferFrom(bob, alice, transferAmount);
+
+        assertEq(ourToken.balanceOf(alice), transferAmount);
+        assertEq(ourToken.balanceOf(bob), STARTING_BALANCE - transferAmount);
+    }
+}
